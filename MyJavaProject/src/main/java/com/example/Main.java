a52db763ed6af7d396f6430b816cdffb82156482
Giri1510 <giriket108@gmail.com>
2025-04-21 13:09:22 +0530

    testtt

---
+++ MyJavaProject/src/main/java/com/example/Main.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = s.nextInt();
+       
+        if (num % 2 == 0) {
+            System.out.println(num + " is an Even number");                 
+        } else {
+            System.out.println(num + " is an Odd number");
+        }
+
+        s.close();
+    }
+}
